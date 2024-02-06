e5536e62cdb4c6524e60e351a3c60527e3d33c31
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-02-05 21:17:53 -0600

    Fix URL typo

--- Chrome Extension/script.js
+++ Chrome Extension/script.js
@@ -66,7 +66,7 @@
   // Co-authors: Kate and Lucas
   function unsafeExtension() {
     pageURL = window.location.href;
-    hostname = pageURl.hostname;
+    hostname = pageURL.hostname;
     tld = hostname.substring(hostname.lastIndexOf("."));
     unsafeDomains = ['.cf', '.work', '.ml', '.ga', '.gq', '.fit', '.tk', '.ru', '.to', '.live', '.cn', '.top', '.xyz', '.pw', '.ws', '.cc', '.buzz'];
     if (unsafeDomains.includes(tld)) {

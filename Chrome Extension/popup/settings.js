7c5be8ab2578eb155279b41c4c243a8dcd641933
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-25 19:47:38 -0500

    Start on custom filter list settings

--- Chrome Extension/popup/settings.js
+++ Chrome Extension/popup/settings.js
@@ -1,8 +1,68 @@
+function createFilterElement(filter) {
+    var listItem = document.createElement('li');
+    var label = document.createElement('span');
+    label.innerHTML = filter;
+    listItem.appendChild(label);
+
+    var editButton = document.createElement('button');
+    editButton.innerHTML = 'Edit';
+    listItem.appendChild(editButton);
+
+    var deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'X';
+    listItem.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', () => {
+        listItem.remove();
+    });
+
+    return listItem;
+}
+
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     let activeTab = tabs[0];
 
+    // Load saved filters from storage
+    chrome.storage.sync.get(['filters'], (data) => {
+        var savedFilters = data.filters;
+
+        if (savedFilters) {
+            var filterList = document.getElementById('filter-list');
+
+            savedFilters.forEach((filter) => {
+                var newFilter = createFilterElement(filter);
+                filterList.appendChild(newFilter);
+            });
+        }
+    });
+
+    document.getElementById('add-filter').addEventListener('click', function() {
+        var filter = document.getElementById('new-filter').value;
+        var filterList = document.getElementById('filter-list');
+
+        if (filter) {
+            var newFilter = createFilterElement(filter);
+            filterList.appendChild(newFilter);
+        }
+
+        document.getElementById('new-filter').value = '';
+    });
+
     // Event listener for confirm button click
     document.getElementById('confirmSettings').addEventListener('click', function() {
+
+        // Save filter list
+        var filters = [];
+        var filterList = document.getElementById('filter-list');
+        filterList.childNodes.forEach((filter) => {
+            var label = filter.querySelector('span');
+            filters.push(label.innerHTML);
+        });
+
+        // Save filters to storage
+        chrome.storage.sync.set({ filters: filters });
+
+
         var ratingInput = document.getElementById('ratingRange');
         var selectedRating = parseInt(ratingInput.value);
         
